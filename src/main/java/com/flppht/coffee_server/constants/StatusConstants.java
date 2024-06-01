9da6dbaa538e951912d7462d7373d350c288b571
flppht <kulasinacfilip@gmail.com>
2024-06-01 17:46:16 +0200

    added class for status constants

--- src/main/java/com/flppht/coffee_server/constants/StatusConstants.java
+++ src/main/java/com/flppht/coffee_server/constants/StatusConstants.java
@@ -1,6 +1,6 @@
 package com.flppht.coffee_server.constants;
 
-public class StatusConstants {
+public final class StatusConstants {
     public static final String PENDING = "pending";
     public static final String IN_PROGRESS = "in progress";
     public static final String COMPLETED = "completed";
